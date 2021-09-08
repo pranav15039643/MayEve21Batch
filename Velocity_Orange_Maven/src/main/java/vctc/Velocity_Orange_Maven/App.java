fe9df5bc6d5a430b66852503ad7f6a45517a7a32
pranav15039643 <49635828+pranav15039643@users.noreply.github.com>
2021-09-08 15:14:22 +0530

    Created maven

---
+++ Velocity_Orange_Maven/src/main/java/vctc/Velocity_Orange_Maven/App.java
@@ -0,0 +1,13 @@
+package vctc.Velocity_Orange_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
